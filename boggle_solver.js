124cd221239d957b0fef3131dbad9cfe14672bc1
andrejwilliams13 <83249443+andrejwilliams13@users.noreply.github.com>
2022-09-29 23:31:22 -0400

    Starter Project #2

--- boggle_solver.js
+++ boggle_solver.js
@@ -1,6 +1,5 @@
 // Andre Williams @02981943
 
-
 /**
  * Given a Boggle board and a dictionary, returns a list of available words in
  * the dictionary present inside of the Boggle board.
@@ -9,114 +8,98 @@
  * @returns {string[]} solutions - Possible solutions to the Boggle board.
  */
 
- exports.findAllSolutions = function(grid, dictionary) {
+exports.findAllSolutions = function (grid, dictionary) {
   let solutions = [];
 
-  if(grid == null || dictionary == null)
-    return soultions;
+  if (grid == null || dictionary == null)
+    return solutions;
 
   let a = grid.length;
-  for(let i = 0; i < a; i ++) {
-    if(grid.length[i] =! a) {
+  for (let i = 0; i < a; i++) {
+    if (grid[i].length != a) {
       return solutions;
     }
   }
 
   convert_to_lowercase(grid, dictionary);
 
-  if(!valid_grid(grid)) {
-    return solutions;
-  }
+  let hashMap = createHashMap(dictionary);
 
   let solutionSet = new Set();
 
-  let hashMap = create_hash_map(dictionary);
-
-  for(let x = 0; x < a; x++) {
-    for(let y = 0; y < a; y++) {
+  for (let y = 0; y < a; y++) {
+    for (x = 0; x < a; x++) {
       let word = "";
       let visited = new Array(a).fill(false).map(() => new Array(a).fill(false));
-      find_words(word, x, y, grid, visited, hashMap, solutionSet);
+      findWords(word, y, x, grid, visited, hashMap, solutionSet);
     }
   }
 
   solutions = Array.from(solutionSet);
 
   return solutions;
-}
+};
 
-find_words = function(word, x, y, grid, visited, hashMap, solutionSet) {
-  let adjMatrix = [[-1, 1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
-  
-  if(x < 0 || y < 0 || x >= grid.length || visited[x][y] == true)
-    return;
-
-  word += grid[x][y];
-  if(prefix(word, hashMap)) {
-    visited[x][y] = true;
-    if(valid_word(word, hashMap)) {
-      if(word.length >= 3)
-        solutionSet.add(word);
-    }
-    for(let i = 0; i < 8; i++) {
-      find_words(word, x + adjMatrix[i][0], y + adjMatrix[i][1], grid, visited, hashMap, solutionSet);
+convert_to_lowercase = function (grid, dict) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      grid[i][j] = grid[i][j].toLowerCase();
     }
   }
-  visited[x][y] = false;
-}
+  for (let i = 0; i < dict.length; i++) {
+    dict[i] = dict[i].toLowerCase();
+  }
+};
 
-prefix = function(word, hashMap) {
+findWords = function (word, y, x, grid, visited, hashMap, solutionSet) {
+  let adjMatrix = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1],
+  ];
+
+  if (y < 0 || x < 0 || y >= grid.length || x >= grid.length || visited[y][x] == true)
+    return;
+
+  word += grid[y][x];
+  if (isPrefix(word, hashMap)) {
+    visited[y][x] = true;
+    if (isWord(word, hashMap)) {
+      if (word.length >= 3)
+        solutionSet.add(word);
+    }
+
+    for (let i = 0; i < 8; i++) {
+      findWords(word, y + adjMatrix[i][0], x + adjMatrix[i][1], grid, visited, hashMap, solutionSet);
+    }
+  }
+  visited[y][x] = false;
+};
+
+isPrefix = function (word, hashMap) {
   return hashMap[word] != undefined;
-}
+};
 
-valid_word = function(word, hashMap) {
+isWord = function (word, hashMap) {
   return hashMap[word] == 1;
-}
+};
 
-create_hash_map = function(dictionary) {
+createHashMap = function (dictionary) {
   var dict = {};
-  for(let i = 0; i < dictionary.length; i++) {
+  for (let i = 0; i < dictionary.length; i++) {
     dict[dictionary[i]] = 1;
     let wordlength = dictionary[i].length;
     var str = dictionary[i];
-    for(let j = wordlength; wordlength > 1; wordlength--) {
-      str = str.substr(0, wordlength-1);
-      if(str in dict) {
-        if(str == 1) {
+    for (let j = wordlength; wordlength > 1; wordlength--) {
+      str = str.substr(0, wordlength - 1);
+      if (str in dict) {
+        if (str == 1) {
           dict[str] = 1;
         }
-      }
-      else {
+      } else {
         dict[str] = 0;
       }
     }
   }
   return dict;
-}
-
-convert_to_lowercase = function(grid, dict) {
-  for(let i = 0; i < grid.length; i++) {
-    for(let j = 0; j < grid[i].length; j++) {
-      grid[i][j] = grid[i][j].toLowerCase();
-    }
-  }
-  for(let i = 0; i < dict.length; i++) {
-    dict[i] = dict[i].toLowerCase();
-  }
-}
-
-valid_grid = function(grid) {
-  regex = /(st|qu)|[a-prt-z]/;
-  for(let i = 0; i < grid.length; i++) {
-    for(let j = 0; j < grid.length[i]; j++) {
-      if(!grid[i][j].match(regex)) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
+};
 
 var grid = [['T', 'W', 'Y', 'R'],
             ['E', 'N', 'P', 'H'],
